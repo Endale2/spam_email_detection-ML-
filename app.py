6aa489fb7221623cd21d5c31fe001dcb0b542f49
Endale2 <endale406@gmail.com>
2025-05-10 23:31:49 +0300

    updated

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# app.py
 from flask import Flask, render_template, request
 import joblib
 import os
 
 app = Flask(__name__)
-model = joblib.load("spam_classifier.pkl")
+
+def load_model():
+    return joblib.load("spam_classifier.pkl")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -12,6 +13,7 @@ def index():
     input_text = ""
     if request.method == "POST":
         input_text = request.form["email_text"]
+        model = load_model()
         result = model.predict([input_text])[0]
         prediction = "Spam" if result == 1 else "Not Spam"
     return render_template("index.html", prediction=prediction, email_text=input_text)
